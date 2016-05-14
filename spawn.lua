449f07e601ffa2ca4edf2552d12aa4ddcec6d7dc
TenPlus1 <kinsellaja@yahoo.com>
2016-05-14 20:25:53 +0100

    change spawn chances

--- spawn.lua
+++ spawn.lua
@@ -8,29 +8,32 @@ mobs:spawn_specific("nssm:ant_worker", {"nssm:ant_dirt"}, {"air"},
 	0, 20, 5, 10, 5, -31000, 31000)
 
 -- SPIDERS
-mobs:spawn_specific("nssm:tarantula", {"default:jungle_grass", "nssm:web" }, {"air"},
-	0, 14, 120, 5000, 3, -31000, 31000)
-mobs:spawn_specific("nssm:uloboros", {"default:jungle_grass", "nssm:web" }, {"air"},
+mobs:spawn_specific("nssm:tarantula", {"default:jungle_grass", "default:jungletree", "nssm:web" }, {"air"},
+	0, 14, 120, 1000, 3, -31000, 31000)
+mobs:spawn_specific("nssm:uloboros", {"default:jungle_grass", "default:jungletree", "nssm:web" }, {"air"},
 	0, 20, 20, 1000, 3, -31000, 31000)
 
 -- SEA
 mobs:spawn_specific("nssm:crocodile", {"default:sand","default:water_source"}, {"air"},
-	0, 20, 60, 35000, 2, -31000, 31000)
+	0, 20, 60, 7000, 2, -31000, 31000)
 mobs:spawn_specific("nssm:octopus", {"default:water_source"}, {"default:water_source"},
-	0, 20, 60, 40000, 2, -31000, 0)
+	0, 20, 60, 20000, 2, -31000, 0)
 
 -- DESERT
 mobs:spawn_specific("nssm:sandworm", {"default:desert_sand", "default:desert_stone"}, {"air"},
-	0, 20, 20, 30000, 2, -31000, 31000)
+	0, 20, 20, 9000, 2, -31000, 31000)
 
 -- MOUNTAINS
 mobs:spawn_specific("nssm:werewolf", {"default:dirt_with_grass"}, {"default:dirt_with_grass"},
-	0, 10, 30, 20000, 2, 20, 31000)
+	0, 10, 30, 9000, 2, 20, 31000)
 
 -- ICE
 mobs:spawn_specific("nssm:white_werewolf", {"default:dirt_with_snow","default:snow"}, {"air"},
-	0, 20, 60, 20000, 2, -31000, 31000)
+	0, 20, 60, 9000, 2, -31000, 31000)
 
 -- SKY
 mobs:spawn_specific("nssm:moonheron", {"air"}, {"air"},
-	0, 10, 100, 750000, 2, 10, 40)
+	0, 10, 100, 80000, 2, 10, 40)
+
+--mobs:spawn_specfic(name, nodes, neighbors, min_light, max_light, interval, chance,
+--	active_object_count, min_height, max_height, day_toggle)
